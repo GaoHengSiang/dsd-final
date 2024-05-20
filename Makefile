3f4ce52b8ce2eb7c83f73609dbecbe9ba8967065
2011eric <201165r1c@gmail.com>
2024-05-20 13:43:51 +0800

    Add scripts for synthesis and running tests

---
+++ Makefile
@@ -0,0 +1,20 @@
+.PHONY: all clean
+
+TB_DEFINE := noHazard
+CYCLE := 10
+TB = Src/Final_tb.v Src/slow_memory.v
+SRC = Src/CHIP.v Src/cache.v
+SYN_SRC = Syn/CHIP_syn.v 
+TSMC13=/home/raid7_2/course/cvsd/CBDK_IC_Contest/CIC/Verilog/tsmc13.v
+
+export 
+
+all: clean syn tb_rtl tb_syn
+syn:
+	dc_shell-t -f syn.tcl
+tb_rtl:
+	make -C Src/ rtl
+tb_syn:
+	make -C Src/ syn
+clean:
+	rm -rf Syn/ 
---
+++ syn.tcl
@@ -0,0 +1,24 @@
+
+set DESIGN CHIP
+set src [list Src/CHIP.v Src/cache.v]
+
+sh mkdir -p Work
+sh mkdir -p Report
+sh mkdir -p Syn
+
+define_design_lib WORK -path Work
+
+analyze -format verilog "$DESIGN.v"
+elaborate $DESIGN
+current_design $DESIGN
+
+source -echo -verbose CHIP_syn.sdc 
+compile_ultra
+
+write_sdf -version 2.1  -context verilog -load_delay cell "Syn/${DESIGN}_syn.sdf"
+write -format verilog -hierarchy -output "Syn/${DESIGN}_syn.v"
+write -format ddc -hierarchy -output "Syn/${DESIGN}_syn.ddc"
+
+report_area -hierarchy > "Report/$DESIGN.area"
+report_timing > "Report/$DESIGN.timing"
+check_design
