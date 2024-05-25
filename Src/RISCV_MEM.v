0e28a6b1aa79abbf50200228df3d1f47a72a296e
Hans Gao <gaohengsiang@gmail.com>
2024-05-25 12:36:32 +0800

    added IO ports

--- Src/RISCV_MEM.v
+++ Src/RISCV_MEM.v
@@ -5,28 +5,70 @@ module MEM_STAGE #(
     )(
     input clk,
     input rst_n,
-    //PIPELINE INPUT FROM ID/EX REGISTER
-    output [BIT_W-1: 0] alu_result,
-    output [BIT_W-1: 0] second_opr,
-    output [4: 0] rd_in
+    //PIPELINE INPUT FROM EX/MEM REGISTER
+    input [BIT_W-1: 0] alu_result_in,
+    input [BIT_W-1: 0] second_opr_in,
+    input [BIT_W-1: 0] nxPC_in,
         //various control signals input
     input memrd_in,
     input memwr_in,
         //transparent
+    input [4: 0] rd_in,
     input mem2reg_in,
     input regwr_in,
 
-
-    //PIPELINE OUTPUT TO EX/MEM REGISTER
-    output [BIT_W-1: 0] alu_result,
-    output [BIT_W-1: 0] second_opr,
+    
+    //PIPELINE OUTPUT TO MEM/WB REGISTER
+    output [BIT_W-1: 0] nx_PC,
+    output [BIT_W-1: 0] alu_result_out,
+    output [BIT_W-1: 0] mem_dat,
     output [4: 0] rd_out,
         //various control signals output
-    output memrd_out,
-    output memwr_out,
     output mem2reg_out,
-    output regwr_out
+    output regwr_out,
+
+    //D_CACHE_INTERFACE
+    input         DCACHE_stall,
+    output        DCACHE_ren,
+    output        DCACHE_wen,
+    output [29:0] DCACHE_addr, //assume word address
+    input  [31:0] DCACHE_rdata,
+    output [31:0] DCACHE_wdata
     //INPUT FROM STANDALONE MODULES SUCH AS FORWARDING, HAZARD_DETECTION
     //maybe no need because forwarding is already done in ID stage
 );
+
+    //Reg and Wire declaration
+    reg [BIT_W-1: 0] nx_PC_r, alu_result_r, mem_dat_r;
+    reg [4: 0] rd_r;
+    reg mem2reg_r, regwr_r;
+    reg [BIT_W-1: 0] nx_PC_w, alu_result_w, mem_dat_w;
+    reg [4: 0] rd_w;
+    reg mem2reg_w, regwr_w;
+    //Continuous assignments
+    assign //to dcache interface
+
+    //module instantiantion
+
+    //Combinational
+
+    //Sequential
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n) begin
+            nx_PC_r         <= 0;
+            alu_result_r    <= 0;
+            mem_dat_r       <= 0;
+            rd_r            <= 0;
+            mem2reg_r       <= 0;
+            regwr_r         <= 0;
+        end
+        else begin
+            nx_PC_r         <= nx_PC_w;
+            alu_result_r    <= alu_result_w;
+            mem_dat_r       <= mem_dat_w;
+            rd_r            <= rd_w;
+            mem2reg_r       <= mem2reg_w;
+            regwr_r         <= regwr_w;
+        end
+    end
 endmodule
