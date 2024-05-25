e3a21c61b7c60f6bf815e9e6fba83f84c7614690
Hans Gao <gaohengsiang@gmail.com>
2024-05-25 19:02:46 +0800

    mostly done

--- Src/RISCV_MEM.v
+++ Src/RISCV_MEM.v
@@ -40,38 +40,59 @@ module MEM_STAGE #(
 );
 
     //Reg and Wire declaration
-    reg [BIT_W-1: 0] nx_PC_r, alu_result_r, mem_dat_r;
-    reg [4: 0] rd_r;
-    reg mem2reg_r, regwr_r;
-    reg [BIT_W-1: 0] nx_PC_w, alu_result_w, mem_dat_w;
-    reg [4: 0] rd_w;
-    reg mem2reg_w, regwr_w;
+    reg [BIT_W-1: 0] PC_plus_4_r, PC_plus_4_w,
+                    alu_result_r, alu_result_w,
+                    mem_dat_r, mem_dat_w;
+    reg [4: 0] rd_r, rd_w;
+    reg mem2reg_r, mem2reg_w, 
+        regwr_r, regwr_w;
+
     //Continuous assignments
     //to dcache interface
     assign DCACHE_ren = memrd_in;
     assign DCACHE_wen = memwr_in;
-    assign DCACHE_addr = 
-    assign DCACHE_rdata = 
-    assign DCACHE_wdata = 
+    assign DCACHE_addr = alu_result_in[31: 2];
+    assign DCACHE_wdata = mem_wdata_in;
+
+    //to pipeline
+    assign alu_result_out = alu_result_r;
+    assign mem_dat = mem_dat_r;
+    assign PC_plus_4_out = PC_plus_4_r;
+    assign rd_out = rd_r;
+    assign mem2reg_out = mem2reg_r;
+    assign regwr_out = regwr_r;
+
+    //other
+    assign d_cache_stall_out = DCACHE_stall;
 
     //module instantiantion
+    //none
 
-    //Combinational
+    //Combinational 
+    always @(*) begin
+        //default
+        alu_result_w    = DCACHE_stall? alu_result_r: alu_result_in;
+        mem_dat_w       = DCACHE_rdata;
+        PC_plus_4_w     = DCACHE_stall? PC_plus_4_r: PC_plus_4_in;
+        rd_w            = DCACHE_stall? rd_r: rd_in;
+        mem2reg_w       = DCACHE_stall? mem2reg_r: mem2reg_in;
+        regwr_w         = DCACHE_stall? regwr_r: regwr_in;
+    end
 
     //Sequential
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
-            nx_PC_r         <= 0;
             alu_result_r    <= 0;
             mem_dat_r       <= 0;
+            PC_plus_4_r     <= 0;
             rd_r            <= 0;
             mem2reg_r       <= 0;
             regwr_r         <= 0;
         end
         else begin
-            nx_PC_r         <= nx_PC_w;
             alu_result_r    <= alu_result_w;
             mem_dat_r       <= mem_dat_w;
+            PC_plus_4_r     <= PC_plus_4_w;
             rd_r            <= rd_w;
             mem2reg_r       <= mem2reg_w;
             regwr_r         <= regwr_w;
