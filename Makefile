0006b855071fcd80eed9942b915bb0a37dd74c57
2011eric <201165r1c@gmail.com>
2024-05-26 16:55:16 +0800

    Add wire connection for IF and ID stage

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 TB_DEFINE := noHazard
 CYCLE := 10
 TB = Src/Final_tb.v Src/slow_memory.v
-SRC = Src/CHIP.v Src/cache.v Src/alu.v Src/register_file.v Src/RISCV_Pipeline.v Src/RISCV_IF.v Src/RISCV_ID.v Src/RISCV_EX.v Src/RISCV_MEM.v Src/RISCV_WB.v
+SRC = Src/CHIP.v Src/cache.v Src/alu.v Src/decoder.v Src/register_file.v Src/RISCV_Pipeline.v Src/RISCV_IF.v Src/RISCV_ID.v Src/RISCV_EX.v Src/RISCV_MEM.v Src/RISCV_WB.v
 SYN_SRC = Syn/CHIP_syn.v 
 TSMC13=/home/raid7_2/course/cvsd/CBDK_IC_Contest/CIC/Verilog/tsmc13.v
 
--- Src/RISCV_Pipeline.v
+++ Src/RISCV_Pipeline.v
@@ -19,9 +19,30 @@ module RISCV_Pipeline(
     output [31:0] PC
     );
 
-    // wire and reg
+    
+    //---------IF stage---------
+    wire         IF_stall;
+    wire         IF_flush;
+    wire  [2:0]  IF_pc_src;
+    wire  [31:0] IF_ID_inst_ppl;
+    wire  [31:0] IF_ID_pc_ppl;
+
+    //TODO: finish this 
+    assign IF_pc_src = {ID_branch_taken, // };
+
+    //---------ID stage---------
+    wire         ID_stall, ID_flush;
     wire [4:0] ID_regfile_rs1, ID_regfile_rs2;
     wire [31:0] ID_regfile_rs1_data, ID_regfile_rs2_data;
+    wire        ID_branch_taken;
+    wire [31:0] ID_pc_branch;
+
+    wire  [4:0] ID_EX_rd_ppl;
+    wire [31:0] ID_EX_rs1_data_ppl, ID_EX_rs2_data_ppl;
+    wire [31:0] ID_EX_imm_ppl;
+    wire        ID_EX_alu_src_ppl;
+    wire [31:0] ID_EX_pc_ppl_out;
+
 
     register_file reg_file(
         .clk(clk),
@@ -34,5 +55,48 @@ module RISCV_Pipeline(
         .rddata1(ID_regfile_rs1_data),
         .rddata2(ID_regfile_rs2_data)
     );
-    
+
+    RISCV_IF IF(
+        .clk(clk),
+        .rst_n(rst_n),
+        .stall(IF_stall),
+        .flush(IF_flush),
+        .pc_src(IF_pc_src),
+        .pc_branch(ID_pc_branch),
+        .pc_j(), // TODO: Connect to EX stage JAL or JALR result
+        .ICACHE_stall(ICACHE_stall),
+        .ICACHE_ren(ICACHE_ren),
+        .ICACHE_wen(ICACHE_wen),
+        .ICACHE_addr(ICACHE_addr),
+        .ICACHE_rdata(ICACHE_rdata),
+        .ICACHE_wdata(ICACHE_wdata),
+        .inst_ppl(IF_ID_inst_ppl), 
+        .pc_ppl(IF_ID_pc_ppl) 
+    );
+
+    RISCV_ID ID(
+        .clk(clk),
+        .rst_n(rst_n),
+        .stall(ID_stall),
+        .flush(ID_flush),
+        .inst_ppl(IF_ID_inst_ppl),
+        .pc_ppl(IF_ID_pc_ppl),
+        .rd_ppl(ID_EX_rd_ppl),
+        .rs1_data_ppl(ID_EX_rs1_data_ppl),
+        .rs2_data_ppl(ID_EX_rs2_data_ppl),
+        .imm_ppl(ID_EX_imm_ppl),
+        .alu_src_ppl(ID_EX_alu_src_ppl),
+        .pc_ppl_out(ID_EX_pc_ppl_out),
+        //----------register_file interface-------------
+        .regfile_rs1(ID_regfile_rs1),
+        .regfile_rs2(ID_regfile_rs2),
+        .regfile_rs1_data(ID_regfile_rs1_data),
+        .regfile_rs2_data(ID_regfile_rs2_data),
+        //----------PC generation-------------------------
+        .branch_taken(ID_branch_taken),
+        .pc_branch(ID_pc_branch)
+    );
+
+
+
 endmodule
