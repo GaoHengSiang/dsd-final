6f58bb715db2a7492bd329744161dad7aafce319
2011eric <201165r1c@gmail.com>
2024-05-26 16:23:45 +0800

    Instantiate regfile in RISCV_Pipeline.v

--- Src/RISCV_Pipeline.v
+++ Src/RISCV_Pipeline.v
@@ -19,6 +19,20 @@ module RISCV_Pipeline(
     output [31:0] PC
     );
 
+    // wire and reg
+    wire [4:0] ID_regfile_rs1, ID_regfile_rs2;
+    wire [31:0] ID_regfile_rs1_data, ID_regfile_rs2_data;
 
-
+    register_file reg_file(
+        .clk(clk),
+        .rst_n(rst_n),
+        .rs1(ID_regfile_rs1),
+        .rs2(ID_regfile_rs2),
+        .rd(), //TODO: handle rd from WB stage
+        .wen(),
+        .wrdata(),
+        .rddata1(ID_regfile_rs1_data),
+        .rddata2(ID_regfile_rs2_data)
+    );
+    
 endmodule
