3350dba1fdf895cd421ff734c325300447eb1d13
GaoHengSiang <166783977+GaoHengSiang@users.noreply.github.com>
2024-05-27 22:27:00 +0800

    Pipeline (#7)
    Most wires have been connected, but many bugs are found: ID need to output many more control signals, and MEM need to propagate the jump signal, we also might need to handle jump operations (flush?)

--- Src/RISCV_Pipeline.v
+++ Src/RISCV_Pipeline.v
@@ -16,7 +16,7 @@ module RISCV_Pipeline(
     input  [31:0] DCACHE_rdata,
     output [31:0] DCACHE_wdata,
 //--------------PC-----------------
-    output [31:0] PC
+    output [31:0] PC //what is this one for?
     );
 
     
@@ -27,8 +27,12 @@ module RISCV_Pipeline(
     wire  [31:0] IF_ID_inst_ppl;
     wire  [31:0] IF_ID_pc_ppl;
 
+
     //TODO: finish this 
-    assign IF_pc_src = {ID_branch_taken, // };
+    //NOT IMPLEMENTED YET
+    //assign IF_pc_src = {ID_branch_taken, // }; 
+
+
 
     //---------ID stage---------
     wire         ID_stall, ID_flush;
@@ -37,21 +41,46 @@ module RISCV_Pipeline(
     wire        ID_branch_taken;
     wire [31:0] ID_pc_branch;
 
+    //-------ID/EX pipeline reg------------
     wire  [4:0] ID_EX_rd_ppl;
     wire [31:0] ID_EX_rs1_data_ppl, ID_EX_rs2_data_ppl;
     wire [31:0] ID_EX_imm_ppl;
     wire        ID_EX_alu_src_ppl;
     wire [31:0] ID_EX_pc_ppl_out;
 
+    //------EX/MEM pipeline reg------------
+    wire [31: 0] EX_MEM_alu_result;
+    wire [31: 0] EX_MEM_mem_wdata;
+    wire [4: 0] EX_MEM_rd;
+    wire [31: 0] EX_MEM_PC_plus_4;
+    wire EX_MEM_memrd, 
+        EX_MEM_memwr,
+        EX_MEM_mem2reg,
+        EX_MEM_regwr,
+        EX_MEM_jump;
+        
+    //------MEM/WB pipeline reg------------
+    wire [31: 0]    MEM_WB_alu_result,
+                    MEM_WB_mem_dat,
+                    MEM_WB_PC_plus_4;
+    wire [4: 0] MEM_WB_rd;
+    wire MEM_WB_mem2reg, MEM_WB_regwr, MEM_WB_jump;//********************MEM needs to have jump signal
+                                                    //so that PC+4 can be stored into reg
+    
+    //LOOP BACK
+    reg [31: 0] rd_data;
 
     register_file reg_file(
         .clk(clk),
         .rst_n(rst_n),
         .rs1(ID_regfile_rs1),
         .rs2(ID_regfile_rs2),
-        .rd(), //TODO: handle rd from WB stage
-        .wen(),
-        .wrdata(),
+
+         //LOOP BACK FROM WB STAGE
+        .rd(MEM_WB_rd),
+        .wen(MEM_WB_regwr), //looped back from WB stage
+        .wrdata(rd_data),
+
         .rddata1(ID_regfile_rs1_data),
         .rddata2(ID_regfile_rs2_data)
     );
@@ -81,12 +110,16 @@ module RISCV_Pipeline(
         .flush(ID_flush),
         .inst_ppl(IF_ID_inst_ppl),
         .pc_ppl(IF_ID_pc_ppl),
+        //ID/EX
         .rd_ppl(ID_EX_rd_ppl),
         .rs1_data_ppl(ID_EX_rs1_data_ppl),
         .rs2_data_ppl(ID_EX_rs2_data_ppl),
         .imm_ppl(ID_EX_imm_ppl),
         .alu_src_ppl(ID_EX_alu_src_ppl),
         .pc_ppl_out(ID_EX_pc_ppl_out),
+
+        //**********************************************OTHER CONTROLS FOR EX
+
         //----------register_file interface-------------
         .regfile_rs1(ID_regfile_rs1),
         .regfile_rs2(ID_regfile_rs2),
@@ -95,8 +128,98 @@ module RISCV_Pipeline(
         //----------PC generation-------------------------
         .branch_taken(ID_branch_taken),
         .pc_branch(ID_pc_branch)
+        
+        //SHOULD ALSO STALL IF DCACHE STALL
+    );
+
+    EX_STAGE EX(
+        .clk(clk),
+        .rst_n(rst_n),
+    //PIPELINE INPUT FROM ID/EX REGISTER
+        .PC_in(ID_EX_pc_ppl_out),
+        .rs1_dat_in(ID_EX_rs1_data_ppl),
+        .rs2_dat_in(ID_EX_rs2_data_ppl),
+        .imm(ID_EX_imm_ppl),
+        //various control signals input
+        .alusrc_in(ID_EX_alu_src_ppl),
+        .aluctrl_in(), //**************************missing control from ID
+        .jal_in(),
+        .jalr_in(),
+        .DCACHE_stall(DCACHE_stall),
+    //transparent for this stage
+        .rd_in(ID_EX_rd_ppl),
+        .memrd_in(),
+        .memwr_in(),
+        .mem2reg_in(),
+        .regwr_in(),
+
+
+    //PIPELINE OUTPUT TO EX/MEM REGISTER
+        .alu_result(EX_MEM_alu_result),
+        .mem_wdata(EX_MEM_mem_wdata),
+        .rd_out(EX_MEM_rd),
+        .PC_plus_4(EX_MEM_PC_plus_4),
+        //various control signals output
+        .memrd_out(EX_MEM_memrd),
+        .memwr_out(EX_MEM_memwr),
+        .mem2reg_out(EX_MEM_regwr),
+        .regwr_out(EX_MEM_jump),
+        .jump_out(EX_MEM_jump) //********************ERROR, jump should not be register blocked
+                               //should correct PC immediately
+        //another signal should tell rd to store PC+4
+                               
+    //INPUT FROM STANDALONE MODULES SUCH AS FORWARDING, HAZARD_DETECTION
+    //maybe no need because forwarding is already done in ID stage
     );
 
 
+    MEM_STAGE MEM(
+        .clk(clk),
+        .rst_n(rst_n),
+    //PIPELINE INPUT FROM EX/MEM REGISTER
+        .alu_result_in(EX_MEM_alu_result),
+        .mem_wdata_in(EX_MEM_mem_wdata),
+        //various control signals input
+        .memrd_in(EX_MEM_memrd),
+        .memwr_in(EX_MEM_memwr),
+        //transparent
+        .PC_plus_4_in(EX_MEM_PC_plus_4),
+        .rd_in(EX_MEM_rd),
+        .mem2reg_in(EX_MEM_mem2reg),
+        .regwr_in(EX_MEM_regwr),
+        // ********************************************ONE SIGNAL SHOULD INDICATE JUMP
+    
+    //PIPELINE OUTPUT TO MEM/WB REGISTER
+        .alu_result_out(MEM_WB_alu_result),
+        .mem_dat(MEM_WB_mem_dat),
+        .PC_plus_4_out(MEM_WB_PC_plus_4),
+        .rd_out(MEM_WB_rd),
+        //various control signals output
+        .mem2reg_out(MEM_WB_mem2reg),
+        .regwr_out(MEM_WB_regwr),
+
+    //D_CACHE_INTERFACE, output not register blocked
+        .DCACHE_stall(DCACHE_stall),
+        .DCACHE_ren(DCACHE_ren),
+        .DCACHE_wen(DCACHE_wen),
+        .DCACHE_addr(DCACHE_addr), //assume word address
+        .DCACHE_rdata(DCACHE_rdata),
+        .DCACHE_wdata(DCACHE_wdata),
+
+    //I/O FOR STANDALONE MODULES SUCH AS FORWARDING, HAZARD_DETECTION
+        .d_cache_stall() //REDUNDANT OUTPUT
+    );
+
+    always @(*) begin
+        rd_data = MEM_WB_alu_result;
+
+        //****************************maybe we can finally utilize PARALLEL CASE here
+        if(MEM_WB_mem2reg) begin
+            rd_data = MEM_WB_mem_dat;
+        end
+        else if (MEM_WB_jump) begin
+            rd_data = MEM_WB_PC_plus_4;
+        end
+    end
 
 endmodule
