07624a371bc42a8d86f7749fe49b58e4ed0d64c4
Hans Gao <gaohengsiang@gmail.com>
2024-06-14 20:03:47 +0800

    for unknown reasons, putting '*' in the first stage yields better results

--- Src/dum_mul.v
+++ Src/dum_mul.v
@@ -7,23 +7,26 @@ module dum_mul (
     input [31: 0] opB,
     output [31: 0] result//register blocked
 );
-    reg [31: 0] first_mul_r, first_mul_w;
+    reg [31: 0] a_stage1_r, b_stage1_r, a_stage1_w, b_stage1_w;
     reg [31: 0] result_r, result_w;
     assign result = result_r;
 
-     always @(*) begin
-        first_mul_w = stall? first_mul_r: opA * opB;//equivalent to EX/MEM
-        result_w = stall? result_r: first_mul_r;//equivalent to MEM/WB
+    always @(*) begin
+        a_stage1_w = stall? a_stage1_r: opA;
+        b_stage1_w = stall? b_stage1_r: opB;
+        result_w = stall? result_r: a_stage1_r * b_stage1_r;//equivalent to MEM/WB
     end
 
 
     always @(posedge clk) begin
         if(!rst_n) begin
-            first_mul_r <= 0;
+            a_stage1_r <= 0;
+            b_stage1_r <= 0;
             result_r <= 0;
         end
         else begin
-            first_mul_r <= first_mul_w;
+            a_stage1_r <= a_stage1_w;
+            b_stage1_r <= b_stage1_w;
             result_r <= result_w;
         end
     end
